81912d314bb8362d4e5948a3cb04c418860c177d
Nicolaas @ Tappy @ Sunny Side Up <github@sunnysideup.co.nz>
2022-06-04 09:42:03 +1200

    MINOR

--- docs/en/INDEX.md
+++ docs/en/INDEX.md
@@ -1,18 +1,49 @@
 # tl;dr
 
-```php
-$array1 = [
-    'Hello' => 'Aaa',
-];
-$array2 = [
-    'Aaa',
-    'Bbb',
-    'Ccc',
-];
-// create the CSV file - hidden or in public dir ...
-(new ArrayToCSV('mydata.csv', $array1, $maxAge = 86400))->setHiddenFile(true)->createFile();
-(new ArrayToCSV('assets/csv-downloads/myotherdata.csv', $array2, $maxAge = 86400))->setHiddenFile(false)->createFile();
+use SilverStripe\ORM\DataObject;
 
-// redirect to download
-(new ArrayToCSV('mydata.csv', $array1))->setHiddenFile(false)->redirectToFile($currentController);
-(new ArrayToCSV('private.csv', $array2))->setHiddenFile(true)->redirectToFile($currentController);
+```php
+
+class MyPageController extends PageController
+{
+
+    private static $allowed_actions = [
+        'csv' => true,
+        'csvfancy' => true,
+    ];
+
+    public function csv($httpRequest)
+    {
+        // redirect to download
+        (ArrayToCSV::create('mydata.csv', $this->getArray(), 86400))
+            ->redirectToFile($this);
+
+    }
+
+    public function csvfancy($httpRequest)
+    {
+        // here are just some other example uses (you can mix and match here)
+        $obj = MyDataObject::get()->first();
+        # special options
+        (new ArrayToCSV('private.csv', $this->getArray()))
+            ->setHiddenFile(false)
+            ->setHeaders($obj->getFieldLabels())
+            ->setList(MyDataObject::get())
+            ->setHeadersFromClassName(MyDataObject::class)
+            ->redirectToFile($this);
+    }
+
+    protected function getArray() : array
+    {
+        return = [
+            'Hello' => 'Aaa',
+        ];        
+    }
+
+    protected function warmCache()
+    {
+        // create the CSV file - hidden or in public dir ...
+        (new ArrayToCSV('mydata.csv', $this->getArray(), $maxAge = 86400))
+            ->createFile();
+    }
+}
--- src/Api/ArrayToCSV.php
+++ src/Api/ArrayToCSV.php
@@ -15,6 +15,8 @@ use SilverStripe\Control\ContentNegotiator;
 
 use SilverStripe\Core\Config\Config;
 
+use SilverStripe\Core\Injector\Injector;
+
 use SilverStripe\Assets\Filesystem;
 
 use SilverStripe\View\ViewableData;
@@ -22,23 +24,84 @@ use Soundasleep\Html2Text;
 
 use Exception;
 
+use SilverStripe\ORM\SS_List;
+
 class ArrayToCSV extends ViewableData
 {
 
     private static $hidden_download_dir = '_csv_downloads';
 
+    /**
+     * can the csv file be accessed directly or only via controller?
+     * @var bool
+     */
     protected $hiddenFile = false;
 
+    /**
+     * "parent" controller
+     * @var Controller|null
+     */
     protected $controller = null;
 
+    /**
+     * name of the file - e.g. hello.csv OR hello/foo/bar.csv OR assets/uploads/tmp.csv
+     * @var string
+     */
     protected $fileName = '';
 
+    /**
+     * any array up to two levels deep
+     * @var array
+     */
     protected $array = [];
 
+    /**
+     * headers for CSV
+     * formatted like this:
+     * "Key" => "Label"
+     * @var array
+     */
+    protected $headers = [];
+
+    /**
+     * how many seconds before the file is stale?
+     * @var int
+     */
     protected $maxAgeInSeconds = 86400;
 
+    /**
+     * internal
+     * @var bool
+     */
     private $infiniteLoopEscape = false;
 
+    /**
+     * typical array is like this:
+     * ```php
+     *     [
+     *         [
+     *             "Key1" => "Value1"
+     *             "Key2" => "Value2"
+     *             "Key3" => "Value3"
+     *         ].
+
+     *         [
+     *             "Key1" => "Value1"
+     *             "Key2" => "Value2"
+     *             "Key3" => "Value3"
+     *         ].
+     *
+     *         [
+     *             "Key1" => "Value1"
+     *             "Key2" => "Value2"
+     *             "Key3" => "Value3"
+     *         ].
+     *     ]
+     * ```
+     * @param string  $fileName         name of the file - e.g. hello.csv OR hello/foo/bar.csv OR assets/uploads/tmp.csv
+     * @param array   $array            any array
+     * @param integer $maxAgeInSeconds  how long before the file is stale
+     */
     public function __construct(string $fileName, array $array, ?int $maxAgeInSeconds = 86400)
     {
         $this->fileName = $fileName;
@@ -46,10 +109,58 @@ class ArrayToCSV extends ViewableData
         $this->maxAgeInSeconds = $maxAgeInSeconds;
     }
 
-    public function setHiddenFile(?bool $bool = true)
+    /**
+     * ensures the file itself can not be downloaded directly
+     * @param boolean $bool
+     * @return self
+     */
+    public function setHiddenFile(?bool $bool = true) : self
     {
         $this->hiddenFile = true;
         return $this;
+    }
+
+    /**
+     * e.g.
+     * [
+     *     "Key1" => "Label1"
+     *     "Key2" => "Label2"
+     *     "Key3" => "Label3"
+     * ]
+     * @param array $array
+     * @return self
+     */
+    public function setHeaders(array $array) : self
+    {
+        $this->headers = $array;
+        return $this;
+    }
+
+    /**
+     * e.g.
+     * [
+     *     "Key1" => "Label1"
+     *     "Key2" => "Label2"
+     *     "Key3" => "Label3"
+     * ]
+     * @param array $array
+     * @return self
+     */
+    public function setHeadersFromClassName(string $className) : self
+    {
+        $this->headers = Injector::inst()->get($className)->fieldLabels();
+        return $this;
+    }
+
+    /**
+     *
+     * @param  SS_List $list any type of list - e.g. DataList
+     * @return self
+     */
+    public function setList(SS_List $list) : self
+    {
+        $this->array = $list->toNestedArray();
+        return $list;
     }
 
     public function createFile()
@@ -62,8 +173,10 @@ class ArrayToCSV extends ViewableData
         if($this->isAssoc()) {
             $row = $this->array[0];
             $this->rowCountCheck = count($row);
-            $headers = array_keys($row);
-            fputcsv($file, $headers);
+            if(empty($this->headers)) {
+                $this->headers = array_keys($row);
+            }
+            fputcsv($file, $this->headers);
         }
         foreach ($this->array as $row) {
             $count = count($row);
